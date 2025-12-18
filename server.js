c47b95bbfd05a6357ae9597ff3fdb2f091777f66
Mee <otkirbekusmonaliyev-code>
2025-12-18 20:19:33 +0500

    register is ready for work

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./config/database.js')
+const authRoute = require("./routes/auth.routes.js")
 
 const app = express()
 app.use(express.json())
@@ -7,6 +8,9 @@ app.use(express.json())
 
 async function start() {
     await db()
+    
+    app.use("/auth", authRoute)
+
     app.listen(3000, () => console.log("http://localhost:3000"))
 }
 start()
