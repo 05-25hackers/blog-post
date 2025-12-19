76e98daa7e6c391701ace8705146ffc97d2d4296
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-12-19 17:01:42 +0500

    delete finished

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,47 +1,48 @@
-const { Post } = require("../models/posts.model.js");
-const { User } = require("../models/users.model.js");
+const { Post } = require('../models/posts.model.js')
+const { User } = require('../models/users.model.js')
 
 const CREATE_POST = async (req, res) => {
-  const { title, body } = req.body;
-  const userId = req.user._id;
-  const newData = await Post.create({
-    title,
-    body,
-    user: userId,
-  });
-  await User.findByIdAndUpdate({ _id: userId }, { $push: { posts: newData } });
-  return res.json({
-    message: "Success",
-    data: newData,
-  });
-};
+	const { title, body } = req.body
+	const userId = req.user._id
+	const newData = await Post.create({
+		title,
+		body,
+		user: userId,
+	})
+	await User.findByIdAndUpdate({ _id: userId }, { $push: { posts: newData } })
+	return res.json({
+		message: 'Success',
+		data: newData,
+	})
+}
 const DELETE_POST = async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-      return res.json({ message: "Post topilmadi" });
-    }
-    await Post.findByIdAndDelete(req.params.id);
-    return res.json({ message: "Post ochirildi" });
-  } catch (error) {
-    return res.json({ message: "Server xatoligi", error: error.message });
-  }
-};
+	try {
+		const post = await Post.findById(req.params.id)
+		if (!post) {
+			return res.json({ message: 'Post topilmadi' })
+		}
+		await Post.findByIdAndDelete(req.params.id)
+		return res.json({ message: 'Post ochirildi' })
+	} catch (error) {
+		return res.json({ message: 'Server xatoligi', error: error.message })
+	}
+}
 
-const UPDATE_POST = async(req, res) => {
-    const {title, body} = req.body
-    const _id = req.params.id
-    const updatedUser = await Post.findByIdAndUpdate({_id}, {title, body})
-    if(!updatedUser) res.json({
-        message: "User not found"
-    })
-    res.json({
-        message: "Successfully updated",
-        data: updatedUser
-    })
+const UPDATE_POST = async (req, res) => {
+	const { title, body } = req.body
+	const _id = req.params.id
+	const updatedUser = await Post.findByIdAndUpdate({ _id }, { title, body })
+	if (!updatedUser)
+		res.json({
+			message: 'User not found',
+		})
+	res.json({
+		message: 'Successfully updated',
+		data: updatedUser,
+	})
 }
 module.exports = {
-  DELETE_POST,
-  CREATE_POST,
-  UPDATE_POST,
-};
+	DELETE_POST,
+	CREATE_POST,
+	UPDATE_POST,
+}
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,14 +1,15 @@
-const express = require("express");
+const express = require('express')
 const {
-  CREATE_POST,
-  DELETE_POST,
-} = require("../controllers/posts.controller.js");
-const { checkToken } = require("../middlewares/auth.middleware.js");
+	CREATE_POST,
+    UPDATE_POST,
+	DELETE_POST,
+} = require('../controllers/posts.controller.js')
+const { checkToken } = require('../middlewares/auth.middleware.js')
 
-const route = express.Router();
+const route = express.Router()
 
-route.post("/post", checkToken, CREATE_POST);
-route.delete("/post/:id", checkToken, DELETE_POST);
+route.post('/post', checkToken, CREATE_POST)
+route.delete('/post/:id', checkToken, DELETE_POST)
 route.put('/post/:id', checkToken, UPDATE_POST)
 
 module.exports = route
