c8cdbddafba545e4dcf608c3598f9a65360927f2
koma <koma@koma.koma>
2025-12-20 15:50:04 +0500

    create get comment delete comment finished

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,25 +1,77 @@
-const {Post} = require("../models/posts.model.js")
-const {User} = require("../models/users.model.js")
+const { Post } = require("../models/posts.model.js");
+const { User } = require("../models/users.model.js");
+const { Comment } = require("../models/comments.model.js");
 
-const CREATE_POST = async(req, res) => {
-    const {title, body} = req.body
-    const userId = req.user._id
-    const newData = await Post.create({
-        title,
-        body,
-        user: userId
-    })
-    await User.findByIdAndUpdate({_id: userId}, {$push:  {posts: newData}})
-    return res.json({
-        message: "Success",
-        data: newData
-    })
-    
-}
-const UPDATE_POST = (req, res) => {
-
-}
+const CREATE_POST = async (req, res) => {
+  const { title, body } = req.body;
+  const userId = req.user._id;
+  const newData = await Post.create({
+    title,
+    body,
+    user: userId,
+  });
+  await User.findByIdAndUpdate({ _id: userId }, { $push: { posts: newData } });
+  return res.json({
+    message: "Success",
+    data: newData,
+  });
+};
+const GET_POSTS = async (req, res) => {
+  const data = await Post.find();
+  res.json({
+    message: "Success",
+    data: data,
+  });
+};
+const DELETE_POST = async (req, res) => {
+  const { id } = req.params;
+  await Post.findByIdAndDelete(id);
+  await User.findByIdAndUpdate(req.user._id, { $pull: { posts: id } });
+  res.json({
+    message: "Ochirildi",
+  });
+};
+const UPDATE_POST = async (req, res) => {
+  const { id } = req.params;
+  const { title, body } = req.body;
+  const data = await Post.findByIdAndUpdate(id, { title, body }, { new: true });
+  res.json({
+    message: "Yangilandi",
+    data: data,
+  });
+};
+const CREATE_COMMENT = async (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  const userId = req.user._id;
+  const newComment = await Comment.create({
+    text,
+    user: userId,
+    post: id,
+  });
+  await Post.findByIdAndUpdate(id, { $push: { comment: newComment._id } });
+  await User.findByIdAndUpdate(userId, { $push: { comment: newComment._id } });
+  res.json({
+    message: "Izoh qoshildi",
+    data: newComment,
+  });
+};
+const DELETE_COMMENT = async (req, res) => {
+  const { id } = req.params;
+  const comment = await Comment.findByIdAndDelete(id);
+  if (comment) {
+    await Post.findByIdAndUpdate(comment.post, { $pull: { comment: id } });
+    await User.findByIdAndUpdate(comment.user, { $pull: { comment: id } });
+  }
+  res.json({
+    message: "Izoh o'chirildi",
+  });
+};
 module.exports = {
-    CREATE_POST, 
-    UPDATE_POST
-}
+  CREATE_POST,
+  GET_POSTS,
+  DELETE_POST,
+  UPDATE_POST,
+  CREATE_COMMENT,
+  DELETE_COMMENT,
+};
