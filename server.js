f315d1ecb076088b817872f1465bad8367878ee0
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-12-22 16:56:49 +0500

    user finished

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const { database } = require("./config/db.js");
-const authRoute = require("./routes/auth.routes.js");
-const postRoute = require("./routes/posts.routes.js");
-const commentRoute = require("./routes/comments.routes.js");
-const app = express();
-app.use(express.json());
+const express = require('express')
+const { database } = require('./config/db.js')
+const authRoute = require('./routes/auth.routes.js')
+const postRoute = require('./routes/posts.routes.js')
+const commentRoute = require('./routes/comments.routes.js')
+const app = express()
+app.use(express.json())
 
 async function start() {
-  await database();
+	await database()
 
-  app.use("/auth", authRoute);
-  app.use(postRoute);
+	app.use('/auth', authRoute)
+	app.use(postRoute)
 
-  app.use(commentRoute);
+	app.use(commentRoute)
 
-  app.listen(3000, () => console.log("http://localhost:3000"));
+	app.listen(3000, () => console.log('http://localhost:3000'))
 }
-start();
+start()
